b9bee4399e52a6dc808d128c15fb8d26225646c1
Rosario Di Somma <rosario.disomma@gmail.com>
2016-10-02 23:52:34 +0200

    Clean up
    Change-Id: I17c74bbc3db1f88d203e30c83bfa1940a4561fd2
    Signed-off-by: Rosario Di Somma <rosario.disomma@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,6 @@
-#!/usr/bin/env bash
+find -type l -exec rm {} \;
 
-function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-	source ~/.profile;
-}
-
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+declare -a Dotfiles=('.aliases' '.gitconfig' '.config')
+for item in "${Dotfiles[@]}"; do
+    ln -s .dotfiles/$item $item
+done
