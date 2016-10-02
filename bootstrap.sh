2eeeb70fa72b5db64cbb66ae09693fa264de2da3
Rosario Di Somma <rosario.disomma@gmail.com>
2016-10-02 22:48:51 +0200

    Add bootstrap script
    Change-Id: Idddf83c3fadc00d609eed7084ff23c405e0092b0
    Signed-off-by: Rosario Di Somma <rosario.disomma@gmail.com>

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function doIt() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude ".osx" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE-MIT.txt" \
+		-avh --no-perms . ~;
+	source ~/.profile;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
