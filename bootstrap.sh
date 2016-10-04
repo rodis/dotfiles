2cd9c3f0eef09929e5d7549248e89fd8c86e8000
Rosario Di Somma <rosario.disomma@gmail.com>
2016-10-04 14:06:17 +0200

    Remove bootstrap.sh file
    The file really belongs to the ansible playbook
    
    Change-Id: Ibff3dc42d29e0f3dd6a8d9494807c7a086321e9a
    Signed-off-by: Rosario Di Somma <rosario.disomma@gmail.com>

--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-find $HOME -maxdepth 1 -type l -exec rm {} \;
-
-EXCLUDE='.git liquidprompt ssh-ident bootstrap.sh dotfiles'
-
-for file in `ls -A .dotfiles`; do
-    if echo $EXCLUDE | grep -q -v $file; then
-        ln -s .dotfiles/$file $file
-    fi
-done
