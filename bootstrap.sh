e6259502bcce64e4009b0c5cf72e1c7e1566c478
Rosario Di Somma <rosario.disomma@gmail.com>
2016-10-04 12:18:57 +0200

    Add vitality plugin
    * Also only remove link in the home folder
    
    Change-Id: I1abbc7b67f514f51c7779fd875d441c9bd40ff90
    Signed-off-by: Rosario Di Somma <rosario.disomma@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-find -type l -exec rm {} \;
+find $HOME -maxdepth 1 -type l -exec rm {} \;
 
 declare -a Dotfiles=('.aliases' '.gitconfig' '.config' '.gitmodules' '.vim' '.vimrc')
 for item in "${Dotfiles[@]}"; do
